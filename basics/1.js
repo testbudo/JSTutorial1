d102cf2f38f7d5ea3c82335efa423d7a55b14542
testbudo <buddycode101@gmail.com>
2024-12-16 14:09:32 +0000

    Added data-tupe summary

--- basics/1.js
+++ basics/1.js
@@ -1,1 +1,11 @@
-console.log("JS")
+const accountid = 1234
+let accountemail = "somemail@somemail.com"
+var accountPass = "Collegekumara"
+accountCity = "Jaipur"
+// accountid=345
+// console.table([accountid,accountPass,accountemail,accountPass])
+// console.log(accountCity);
+let accountstat
+console.log(accountstat);
+
+
---
+++ datatypes-summary.js
@@ -0,0 +1,27 @@
+// #2 official types of Data types are 
+
+// 1)Primitive
+// Number,string,bigint,null,undefined,boolean,symbol
+
+// var declrataion has shortcomings so we use let
+
+
+//2)Non primitive/Primitive
+// Arrays objects and functrion
+
+// arrays 
+const heroes=["Thor","Captain"]
+
+
+//objects
+let obj={
+    name:"Shravan",
+    age:15,
+}
+
+//Functions
+
+const Myfunc=function(){
+    console.log("Hello World");
+}
+
